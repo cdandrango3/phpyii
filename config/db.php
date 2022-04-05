11a52a51829d68121181b84577a7f7648c5aade6
Christian <you@example.com>
2022-04-05 16:07:13 -0500

    cartas

--- config/db.php
+++ config/db.php
@@ -2,9 +2,9 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'pgsql:host=localhost;dbname=tre1',
-    'username' => 'postgres',
-    'password' => 'barcelona97.',
+    'dsn' => 'pgsql:host=ec2-54-157-79-121.compute-1.amazonaws.com;dbname=dfn3m087fsipl',
+    'username' => 'rwrphelswemffi',
+    'password' => '434801de2d2ed6e93f7674f6b2d68d4f8e7e2820851db7aa2cb97069ae9f2ae4',
     'charset' => 'utf8',
     'schemaMap' => [
         'pgsql'=> [
