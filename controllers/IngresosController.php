ccf6e7e2583e971d63e2f60925e5752416f6f950
unknown <cdandrango@uce.edu.ec>
2022-04-06 11:30:47 -0500

    sdd

--- controllers/IngresosController.php
+++ controllers/IngresosController.php
@@ -57,8 +57,8 @@ class IngresosController extends Controller
                         $facbody = new FacturaBody;
                         $facbody->cant = 1;
                         $isiva=$i_pro["product_iva_id"];
-                        $facbody->precio_u = ($isiva>0)?$val->Valor-($val->Valor)*$i_pro/100:$val->Valor;
-                        $sumiva+=($isiva>0)?($val->Valor)*($i_pro/100):0;
+                        $facbody->precio_u = ($isiva>0)?$val->Valor-($val->Valor)*($isiva/100):$val->Valor;
+                        $sumiva+=($isiva>0)?($val->Valor)*($isiva/100):0;
                         $facbody->precio_total =$facbody->precio_u * $facbody->cant ;
                         $facbody->id_producto = $i_pro["id"];
                         $facbody->id_head = $headfact->n_documentos;
