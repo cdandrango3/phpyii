6935271e6230054a5a3d424d7a813cd0520d2c13
unknown <cdandrango@uce.edu.ec>
2022-04-07 10:32:05 -0500

    heroku

--- controllers/PersonController.php
+++ controllers/PersonController.php
@@ -12,21 +12,23 @@ public function actionIndex(){
     ob_start();
     ob_implicit_flush(false);
     require('data.json');
-    ini_set('max_execution_time', 300);
     $details = json_decode(ob_get_clean(),true);
 foreach($details as $value){
     $NombreC=$value["Nombre"] ." ".$value["Apellido"] ;
     $Cedula=$value["Cedula"];
     $Correo=$value["Correo"];
     $Telefono=$value["Telefono"];
+    $Adress="Nelson Estupiñan N75-12 y Francisco Granizo";
     $id=$value["id"];
 $person=New Person();
 $person->person_type_id=1;
 $person->name=$NombreC;
 $person->cedula=strval($Cedula);
-$person->phones=strval($Telefono);
+$person->phones=$Telefono;
 $person->institution_id=1;
 $person->city_id=1;
+$person->address=$Adress;
+$person->person_type_id=2;
 $person->special_taxpayer=1;
 $person->id_myhouse=$id;
 $person->emails=$Correo;
